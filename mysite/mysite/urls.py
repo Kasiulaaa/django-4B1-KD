84277b146d8ff26d1e4e55fa45705c8938fbb3af
Kasia Dzieszkowska <katarzynadzieszkowska@gmail.com>
2025-04-30 08:41:09 +0200

    Corrected urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('polls/', include("polls.urls")),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,9 +7,6 @@ from django.utils import timezone
 
 from .models import Choice, Question
 
-
-
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -19,9 +16,8 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
-        :5
-    ]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
@@ -30,7 +26,6 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
-
 
 class ResultsView(generic.DetailView):
     model = Question
